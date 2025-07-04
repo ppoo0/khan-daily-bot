e1856606b67b3e6dfb08b00ab040ea1f08dabab5
ppoo0 <160346337+ppoo0@users.noreply.github.com>
2025-07-05 01:50:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ def start_bot():
         print(f"[!] Telegram Error: {context.error}")
     dp.add_error_handler(error_handler)
     webhook_url = 'https://relaxed-vannie-asew-a4c78a9c.koyeb.app/webhook'
-    updater.start_webhook(listen="0.0.0.0", port=443, url_path="webhook")  # Changed to 443
+    updater.start_webhook(port=443, url_path="webhook")  # Changed to 443
     updater.bot.set_webhook(url=webhook_url)
     print(f"[+] Webhook server started at {webhook_url}")
     return updater
